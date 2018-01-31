fc2a07123650565c643192a130b389501e679e61
Katherine Eaton <ktmeaton@gmail.com>
2018-01-30 19:14:03 -0500

    Plotting

--- scripts/plotWorld.R
+++ scripts/plotWorld.R
@@ -1,18 +1,69 @@
 
 library(ggplot2)
+library(ggthemes)
 
 # Select the file from the file chooser
 fileToLoad <- file.choose(new = TRUE)
 
-# Read in the CSV data and store it in a variable 
-geocode_input <- read.table(fileToLoad, stringsAsFactors = FALSE, header = TRUE, quote="",sep = '\t')
+world <- map_data("world")
+world <- world[world$region != "Antarctica",] # intercourse antarctica
 
-#Using GGPLOT, plot the Base World Map
-mp <- NULL
-mapWorld <- borders("world", colour="gray50", fill="gray50") # create a layer of borders
-mp <- ggplot() +   mapWorld
+# Read in the tsv data and store it in a variable 
+data <- read.table(fileToLoad, header = TRUE, quote="",sep = '\t')
 
-#Now Layer the cities on top
-mp <- mp + geom_point(aes(x=geocode_input$lat, y=geocode_input$lon) ,color="blue", size=1) 
+# Create the consensus count column
+data$consensus_count <- rep(NA, nrow(data))
+
+# Get counts of unique lat and lon elements
+for(i in 1:nrow(data))
+{
+  # Store lat and lon for current row
+  current_lat <- data$consensus_lat[i]
+  current_lon <- data$consensus_lon[i]
+  # Store count of current lat and lon occurring in data frame
+  count_lat <- length(which(data$consensus_lat == current_lat))
+  count_lon <- length(which(data$consensus_lon == current_lon))
+  # populate consensus count column only if lat and lon counts agree
+  if(count_lat == count_lon)
+  {
+    data$consensus_count[i] <- count_lat
+  }
+  else
+  {
+    minimal_count <- min(count_lat, count_lon)
+    data$consensus_count[i] <- minimal_count
+  }
+}
+
+distinct_data <- distinct(data, geographic_location, consensus_lat, consensus_lon, Pandemic, Established, consensus_count)
+
+
+
+
+
+##########################################
+#                PLOTTING                #
+##########################################
+
+# Null ggplot
+gg <- ggplot()
+# Base world map
+gg <- gg + geom_map(data=world, map=world,
+                    aes(x=long, y=lat, map_id=region),
+                    color="white", fill="#7f7f7f", size=0.05, alpha=2/4)
+# Add data points
+gg <- gg + geom_point(data=distinct_data, 
+                      aes(x=consensus_lon, y=consensus_lat, size=consensus_count, 
+                          fill=Established), shape=21, alpha=0.8)
+
+# Tableau color theme
+gg <- gg + scale_color_tableau()
+# Theme: strip background, reposition legend
+gg <- gg + theme_map()
+gg <- gg + theme(strip.background=element_blank())
+gg <- gg + guides(fill=FALSE)
+gg <- gg + guides(size=FALSE)
+
+
 
 
