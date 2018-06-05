3bf8e94c7279ef48763da9c1e8e7641b511beaea
Katherine Eaton <ktmeaton@gmail.com>
2018-06-05 19:53:31 -0400

    Dynamic play

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
 OUTPUT_DIR = "."
 EMAIL = "ktmeaton@gmail.com"
 DATABASE = "yersinia_pestis_db.sqlite"
-TABLES = ["Assembly"]
+TABLES = ["Assembly","BioSample"]
 SEARCH_TERMS = {"Assembly": "Yersinia pestis[Orgn]",
-				"BioSample": "Yersinia pestis[Orgn]"}
+                "BioSample": "Yersinia pestis[Orgn]"}
 
 TABLE_COLUMNS = {"Assembly": ["AssemblyAccession",
-							  "Organism",
-							  "AssemblyStatus",
-							  "SubmitterOrganization",
-							  "Taxid",
-							  "BioSampleAccn",
-							  "Coverage",
-							  "SubmissionDate",
-							  "SeqReleaseDate",
-							  "WGS",
-							  "BioprojectAccn",
-							  "chromosome_count"],
-				 "BioSample": ["accession"]}
+                              "Organism",
+                              "AssemblyStatus",
+                              "SubmitterOrganization",
+                              "Taxid",
+                              "BioSampleAccn",
+                              "Coverage",
+                              "SubmissionDate",
+                              "SeqReleaseDate",
+                              "WGS",
+                              ["GB_BioProjects","BioprojectAccn"],
+                              "chromosome_count",
+                              "contig_count",
+                              ["InfraspeciesList","Biosource","Sub_value"]],
+                 "BioSample": ["Accession"]}
