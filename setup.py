0978441e3703677c39a382bffcc40d27b8d440c2
Katherine Eaton <ktmeaton@gmail.com>
2019-11-27 15:34:28 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+   name='NCBImeta',
+   version='0.4.1',
+   description='Efficient and comprehensive metadata acquisition from the NCBI databases (includes SRA).',
+   python_requires='>=3',
+   license="MIT",
+   long_description=long_description,
+   author='Katherine Eaton',
+   author_email='ktmeaton@gmail.com',
+   url="https://github.com/ktmeaton/NCBImeta",
+   packages=setuptools.find_packages(),
+   classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+   ],
+   install_requires=['biopython==1.74', 'PyYAML==5.1.2', 'numpy==1.16.4'], #external packages as dependencies
+   scripts=[
+      'src/NCBImeta.py',
+      'src/NCBImetaExport.py',
+      'src/NCBImetaJoin.py',
+      'src/NCBImetaAnnotateReplace.py',
+      'src/NCBImetaAnnotateConcatenate.py'
+   ]
+)
