f3b30de3980841da269ae5f449a524a6b4f96f8c
Katherine Eaton <ktmeaton@gmail.com>
2019-12-04 23:27:56 -0500

    v0.5.0 version update [skip ci]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", 'r') as r:
 
 setuptools.setup(
    name='NCBImeta',
-   version='0.4.2',
+   version='0.5.0',
    description='Efficient and comprehensive metadata acquisition from the NCBI databases (includes SRA).',
    python_requires='>=3',
    license="MIT",
