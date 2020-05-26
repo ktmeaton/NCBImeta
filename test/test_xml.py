000d90603a14db8e124fdfe9e69829cdb920e210
Katherine Eaton <ktmeaton@gmail.com>
2020-05-26 12:17:16 -0400

    lint test_xml

--- test/test_xml.py
+++ test/test_xml.py
@@ -1,19 +1,29 @@
-import pytest                               # Testing suite
-from ncbimeta import NCBImetaUtilities      # Utility Functions
-from lxml import etree                      # XML Parsing
+"""
+NCBImeta Test - lxml
+
+@author: Katherine Eaton
+"""
+
+# -----------------------------------------------------------------------#
+#                         Modules and Packages                           #
+# -----------------------------------------------------------------------#
+
+# from ncbimeta import NCBImetaUtilities  # Utility Functions
+from lxml import etree  # XML Parsing
+
 
 def test_lxml(assembly_xml):
-    '''Test the functionality of the lxml module'''
-    column_name =  'AssemblyAccession'
-    column_payload =  ['AssemblyAccession']
+    """Test the functionality of the lxml module"""
+    column_name = "AssemblyAccession"
+    column_payload = ["AssemblyAccession"]
     column_value = ""
-    root = etree.fromstring(assembly_xml, )
-    tag_xpath = './/' + "Meta"
+    root = etree.fromstring(assembly_xml,)
+    tag_xpath = ".//" + "Meta"
     # Step 1 going to iterate through column_payload values
     working_root = root
     column_dict = {}
     # Iterate over each path element in the column_payload list
-    for i in range(0,len(column_payload)):
+    for i in range(0, len(column_payload)):
         # Construct an xpath recursive search query
         tag_xpath = ".//" + column_payload[i]
         # recursively search for the tag
@@ -25,14 +35,14 @@ def test_lxml(assembly_xml):
     column_dict[column_name] = column_value
     print(column_dict)
 
-
-    #print("Findall: ", root.findall(tag_xpath))  # XPath, recursive.
-    #print("Iterfind: ", root.iterfind(tag_xpath))
-    #print("Findtext: ", root.findtext(tag_xpath).strip())
-    #print(assembly_xml)
+    # print("Findall: ", root.findall(tag_xpath))  # XPath, recursive.
+    # print("Iterfind: ", root.iterfind(tag_xpath))
+    # print("Findtext: ", root.findtext(tag_xpath).strip())
+    # print(assembly_xml)
     assert 0
 
-#iterfind() iterates over all Elements that match the path expression
-#findall() returns a list of matching Elements
-#find() efficiently returns only the first match
-#findtext() returns the .text content of the first match
+
+# iterfind() iterates over all Elements that match the path expression
+# findall() returns a list of matching Elements
+# find() efficiently returns only the first match
+# findtext() returns the .text content of the first match
