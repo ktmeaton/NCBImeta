5be6eb63019ad3d1612661da5af431f7d7195518
Katherine Eaton <ktmeaton@gmail.com>
2020-05-28 13:13:20 -0400

    update ver use the post suffix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as r:
 
 setuptools.setup(
     name="NCBImeta",
-    version="0.6.7a0",
+    version="0.6.6-post1",
     description=(
         "Efficient and comprehensive metadata acquisition "
         "from the NCBI databases (includes SRA)."
