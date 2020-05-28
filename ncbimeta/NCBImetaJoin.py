d4e906b2d7cb81dd298c565c0dda5edf80bffcfc
Katherine Eaton <ktmeaton@gmail.com>
2020-05-28 13:19:27 -0400

    commit to post suffix after test

--- ncbimeta/NCBImetaJoin.py
+++ ncbimeta/NCBImetaJoin.py
@@ -79,7 +79,7 @@ mandatory.add_argument(
     required=True,
 )
 
-parser.add_argument("--version", action="version", version="%(prog)s v0.6.6-post1")
+parser.add_argument("--version", action="version", version="%(prog)s v0.6.6.post1")
 
 
 args = vars(parser.parse_args())
