6f383e32b697953f602086574e33aab0d012b07a
Katherine Eaton <ktmeaton@gmail.com>
2020-05-28 00:49:04 -0400

    update version numbers with dev suffix

--- ncbimeta/NCBImetaAnnotateConcatenate.py
+++ ncbimeta/NCBImetaAnnotateConcatenate.py
@@ -62,7 +62,7 @@ mandatory.add_argument(
     required=True,
 )
 
-parser.add_argument("--version", action="version", version="%(prog)s v0.6.6dev")
+parser.add_argument("--version", action="version", version="%(prog)s v0.6.6.dev0")
 
 
 args = vars(parser.parse_args())
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as r:
 
 setuptools.setup(
     name="NCBImeta",
-    version="0.6.6dev",
+    version="0.6.6.dev0",
     description=(
         "Efficient and comprehensive metadata acquisition "
         "from the NCBI databases (includes SRA)."
