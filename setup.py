b63793069e419c7675745dd66ba02135f939f33b
Katherine Eaton <ktmeaton@gmail.com>
2020-08-24 13:02:00 -0400

    update to 0.6.7dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as r:
 
 setuptools.setup(
     name="NCBImeta",
-    version="0.6.6.post1",
+    version="0.6.7dev",
     description=(
         "Efficient and comprehensive metadata acquisition "
         "from the NCBI databases (includes SRA)."
