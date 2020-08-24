18bf0c8da5db414dc3738299384b7a7cb75d3ed6
Katherine Eaton <ktmeaton@gmail.com>
2020-08-24 18:37:39 -0400

    remove dev suffix from version number

--- ncbimeta/NCBImetaAnnotateConcatenate.py
+++ ncbimeta/NCBImetaAnnotateConcatenate.py
@@ -62,7 +62,7 @@ mandatory.add_argument(
     required=True,
 )
 
-parser.add_argument("--version", action="version", version="%(prog)s v0.7.0dev")
+parser.add_argument("--version", action="version", version="%(prog)s v0.7.0")
 
 
 args = vars(parser.parse_args())
