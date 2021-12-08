f2b54b216771f1859c9f91a845c590ac361d189e
Katherine Eaton <ktmeaton@gmail.com>
2021-12-08 16:10:50 -0700

    adjust test workflow for renamed annotate

--- test/test_annotate.py
+++ test/test_annotate.py
@@ -17,8 +17,34 @@ import subprocess  # Execute CLI/Shell
 # -----------------------------------------------------------------------------#
 
 
-def test_annotatereplace_run():
-    """Test the NCBImetaAnnotateReplace application for run completion"""
+def test_annotate_concatenate_run():
+    """Test the NCBImetaAnnotate application in concatenate mode for run completion"""
+    # Use the test database
+    test_db = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.sqlite")
+    test_annotfile = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "test_annot.txt"
+    )
+    # If the test_db doesn't alread exist, run the test cmd from test_ncbimeta
+    if not os.path.exists(test_db):
+        test_ncbimeta.test_ncbimeta_run()
+    test_table = "BioSample"
+    test_cmd = (
+        "ncbimeta/NCBImetaAnnotate --database "
+        + test_db
+        + " --table  "
+        + test_table
+        + " --annotfile "
+        + test_annotfile
+        + " --concatenate"
+    )
+    # test NCBImetaAnnotate through a subprocess
+    returned_value = subprocess.call(test_cmd, shell=True)
+    # If it returns a non-zero value, it failed
+    assert returned_value == 0
+
+
+def test_annotate_replace_run():
+    """Test the NCBImetaAnnotate application for run completion"""
     # Use the test database
     test_db = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.sqlite")
     test_annotfile = os.path.join(
@@ -29,27 +55,27 @@ def test_annotatereplace_run():
         test_ncbimeta.test_ncbimeta_run()
     test_table = "BioSample"
     test_cmd = (
-        "ncbimeta/NCBImetaAnnotateReplace --database "
+        "ncbimeta/NCBImetaAnnotate --database "
         + test_db
         + " --table  "
         + test_table
         + " --annotfile "
         + test_annotfile
     )
-    # test NCBImetaAnnotateReplace through a subprocess
+    # test NCBImetaAnnotate through a subprocess
     returned_value = subprocess.call(test_cmd, shell=True)
     # If it returns a non-zero value, it failed
     assert returned_value == 0
 
 
-# def test_annotateconcatenate_multipleEntry(tmpdir):
+# def test_annotate_multipleEntry(tmpdir):
 # To Be done
 
-# def test_annotatereplace_ErrorDBNotExists(tmpdir):
+# def test_annotate_ErrorDBNotExists(tmpdir):
 # To Be done
 
-# def test_annotatereplace_ErrorAnnotFileNotExists(tmpdir):
+# def test_annotate_ErrorAnnotFileNotExists(tmpdir):
 # To Be done
 
-# def test_annotatereplace_ErrorTableNotInDB(tmpdir):
+# def test_annotate_ErrorTableNotInDB(tmpdir):
 # To Be done
